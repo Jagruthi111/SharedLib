851113f0f2836c9c39b7fffe5f7ea5f66624dfeb
Jagruthi111 <154959968+Jagruthi111@users.noreply.github.com>
2024-01-22 21:52:31 +0530

    Update sshFunction.groovy

--- vars/sshFunction.groovy
+++ vars/sshFunction.groovy
@@ -2,6 +2,6 @@ def ssh(String user, String ip) {
 sh "ssh ${user}@${ip}"
 }
 
-
-  
-            // sh 'scp /home/slave01/workspace/SamplePipeline/target/hello-world-war-1.0.0.war root@172.31.2.55:/opt/apache-tomcat-8.5.98/webapps/'
+def scp(String warPath, String user, String ip, String destPath) {
+  sh "scp ${warPath} ${user}@${ip}:${destPath}"
+}
