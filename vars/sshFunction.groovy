76bf11e3f858ee3631256640ad9052552d873d61
Jagruthi111 <154959968+Jagruthi111@users.noreply.github.com>
2024-01-22 21:05:18 +0530

    Create sshFunction.groovy

---
+++ vars/sshFunction.groovy
@@ -0,0 +1,7 @@
+def ssh(user,ip) {
+sh "ssh ${user}@${ip}"
+}
+
+
+  
+            // sh 'scp /home/slave01/workspace/SamplePipeline/target/hello-world-war-1.0.0.war root@172.31.2.55:/opt/apache-tomcat-8.5.98/webapps/'
