d7d9ebe3472fd25fe879519503e2beb07be30f1b
Jagruthi111 <154959968+Jagruthi111@users.noreply.github.com>
2024-01-23 08:49:36 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,4 +1,4 @@
 def call() {
-sh 'mvn sonar:sonarb-Dsonar.projectkey=sonar -Dsonar.host.url=http://3.111.55.250:9000 -Dsonar=sonar.exclusions*/.java'
+sh 'mvn sonar:sonar -Dsonar.projectkey=sonar -Dsonar.host.url=http://65.0.110.26:9000 -Dsonar=sonar.exclusions*/.java'
 }
 return this
